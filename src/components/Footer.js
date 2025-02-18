1d4480e9e6a62ce23b4849fb94149088367df79d
CaptainBeatty <juliengrange.dev@gmail.com>
2025-02-18 20:09:38 +0100

    footer improvment

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -1,5 +1,8 @@
 import React from "react";
 import { Link } from "react-router-dom";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faEnvelope } from "@fortawesome/free-regular-svg-icons";
+import { faCcPaypal } from "@fortawesome/free-brands-svg-icons"; // Icône PayPal
 import "./Footer.css";
 
 const Footer = () => {
@@ -14,6 +17,21 @@ const Footer = () => {
                 <Link to="./about-liminal" className="footer-link">
                     Who are we ?
                 </Link>
+            </div>
+            <div className="footer-section">
+                <a
+                    href="https://www.paypal.com/donate?hosted_button_id=C3JR8J3FW89XC"
+                    target="_blank"
+                    rel="noopener noreferrer"
+                    className="footer-link paypal-button"
+                >
+                    <FontAwesomeIcon icon={faCcPaypal} /> Donate with PayPal
+                </a>
+            </div>
+            <div className="footer-section">
+                <a href="mailto:webmaster@liminal.com" className="footer-link">
+                    <FontAwesomeIcon icon={faEnvelope} /> Contact Webmaster
+                </a>
             </div>
         </footer>
     );
